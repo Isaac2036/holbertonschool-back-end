01912d36e32431234f739c0999ff2fab45b73953
Isaac2036 <isaacpablo0596@hotmail.com>
2024-02-04 20:22:04 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""
-place holder
-"""
+""" 0-gather_data_from_an_API
 
+    Given employee ID, returns information
+    about his/her todo list progress.
+"""
 import requests
-from sys import argv
+import sys
+
+
+def main():
+    """According to user_id, show information
+    """
+    user_id = sys.argv[1]
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    todos = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
+        user_id)
+    name = requests.get(user).json().get('name')
+    request_todo = requests.get(todos).json()
+    tasks = [task.get('title')
+             for task in request_todo if task.get('completed') is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          len(tasks),
+                                                          len(request_todo)))
+    print('\n'.join('\t {}'.format(task) for task in tasks))
+
 
 if __name__ == "__main__":
-
-    url = "https://jsonplaceholder.typicode.com/"
-
-    employee_id = sys.argv[1]
-
-    user_response = requests.get(url + "users/{}".format(employee_id))
-
-    user_response = user_response.json()
-
-    params = {"userId": employee_id}
-
-    todos_response = requests.get(url + "todos", params=params)
-
-    todos = todos_response.json()
-
-    completed = []
-
-    for todo in todos:
-        if todo.get("completed") is True:
-            completed.append(todo.get"title")
-
-    print("Employee {} is done with tasks ({}/{})".format(user.get("name").len(completed).len(todos)))
-    
-    for complete in completed:
-        print("/t {} ".format(complete))
+    if len(sys.argv) == 2:
+        main()
